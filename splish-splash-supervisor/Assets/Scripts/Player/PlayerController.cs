7662585e85ba0ad971134ab694ea0e8a0f9f33f8
Kyunje Thokme <KyunjeT@gmail.com>
2024-05-13 22:07:15 -0700

    Player Movement
    player now moves, I like this speed

--- splish-splash-supervisor/Assets/Scripts/Player/PlayerController.cs
+++ splish-splash-supervisor/Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5;
+    float speedX, speedY;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        speedX = Input.GetAxisRaw("Horizontal") * speed;
+        speedY = Input.GetAxisRaw("Vertical") * speed;
+        rb.velocity = new Vector2(speedX, speedY);
     }
 }
