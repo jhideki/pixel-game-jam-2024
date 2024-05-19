f47279477925de7345b8c9cd36b98cded7412922
Jonathan Ami <126116089+jonathan-ami@users.noreply.github.com>
2024-05-18 21:16:29 -0700

    update

---
+++ splish-splash-supervisor/Assets/Scripts/NPC/NPCAnimation.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAnimation : MonoBehaviour
+{
+    public List<Sprite> nSprites;
+    public List<Sprite> eSprites;
+    public List<Sprite> sSprites;
+
+    public List<Sprite> sIdleSprites;
+    private List<Sprite> selectedSprites;
+    private SpriteRenderer spriteRenderer;
+    public float frameRate;
+    private float changeX;
+    public float changeCutoff;
+    private float changeY;
+    private Rigidbody2D rb;
+
+    private int facing = 1;// 1 for east, 2 for north, 3 for south
+
+    private Vector3 lastPosition;
+    private Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        selectedSprites = eSprites;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!spriteRenderer.flipX && changeX > 0f)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (spriteRenderer.flipX && changeX < 0f)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if (Mathf.Abs(changeX) > changeCutoff && Mathf.Abs(changeY) > changeCutoff)
+        {
+            SetSprite();
+        }
+        else if (rb.velocity.magnitude > 0.1f)
+        {
+            SetSpriteRB();
+        }
+        else
+        {
+            selectedSprites = sIdleSprites;
+        }
+
+        if (transform.position != lastPosition)
+        {
+            changeX = transform.position.x - lastPosition.x;
+            changeY = transform.position.y - lastPosition.y;
+            direction = (transform.position - lastPosition).normalized;
+        }
+
+        lastPosition = transform.position;
+
+        int frame = (int)(Time.time * frameRate % 5);
+        Debug.Log("----------- fram: " + frame);
+
+        spriteRenderer.sprite = selectedSprites[frame];
+    }
+
+    void SetSprite()
+    {
+        if (Mathf.Abs(changeY) > Mathf.Abs(changeX) && changeY > 0)
+        {
+            selectedSprites = nSprites;
+            facing = 2;
+        }
+        else if (Mathf.Abs(changeY) > Mathf.Abs(changeX) && changeY < 0)
+        {
+            selectedSprites = sSprites;
+            facing = 3;
+        }
+        else if (Mathf.Abs(changeY) < Mathf.Abs(changeX))
+        {
+            selectedSprites = eSprites;
+            facing = 1;
+        }
+    }
+
+    void SetSpriteRB()
+    {
+        if (Mathf.Abs(rb.velocity.x) > Mathf.Abs(rb.velocity.y))
+        {
+            selectedSprites = eSprites;
+            facing = 1;
+        }
+        else if (Mathf.Abs(rb.velocity.x) < Mathf.Abs(rb.velocity.y) && rb.velocity.y > 0)
+        {
+            selectedSprites = nSprites;
+            facing = 2;
+        }
+        else if (Mathf.Abs(rb.velocity.x) < Mathf.Abs(rb.velocity.y) && rb.velocity.y < 0)
+        {
+            selectedSprites = sSprites;
+            facing = 3;
+        }
+    }
+}
---
+++ splish-splash-supervisor/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public List<Sprite> nSprites;
+    public List<Sprite> eSprites;
+    public List<Sprite> sSprites;
+    public List<Sprite> sIdleSprites;
+    public List<Sprite> eIdleSprites;
+    public List<Sprite> nIdleSprites;
+
+    private List<Sprite> selectedSprites;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator anim;
+    public float frameRate;
+    private int facing = 1;// 1 for east, 2 for north, 3 for south
+
+    //Underwater variables
+    private bool playingAnimation;
+    public float animationBuffer = 2.0f;
+    private float appearStartTime;
+    public bool isTimingAppear;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        selectedSprites = eSprites;
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+        playingAnimation = false;
+        isTimingAppear = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Time.time - appearStartTime) > animationBuffer && playingAnimation)
+        {
+            anim.enabled = false;
+            playingAnimation = false;
+            isTimingAppear = false;
+        }
+
+
+        if (!spriteRenderer.flipX && rb.velocity.x > 0f)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (spriteRenderer.flipX && rb.velocity.x < 0f)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        // setsprite if moving else set idle sprite
+        if (rb.velocity.magnitude > 0.1f)
+        {
+
+            SetSprite();
+        }
+        else
+        {
+            selectedSprites = sIdleSprites;
+        }
+        int frame = (int)(Time.time * frameRate % 6);
+        Debug.Log("-------" + frame);
+
+
+        spriteRenderer.sprite = selectedSprites[frame];
+    }
+    void SetSprite()
+    {
+        if (Mathf.Abs(rb.velocity.x) > Mathf.Abs(rb.velocity.y))
+        {
+            selectedSprites = eSprites;
+            facing = 1;
+        }
+        else if (Mathf.Abs(rb.velocity.x) < Mathf.Abs(rb.velocity.y) && rb.velocity.y > 0)
+        {
+            selectedSprites = nSprites;
+            facing = 2;
+        }
+        else if (Mathf.Abs(rb.velocity.x) < Mathf.Abs(rb.velocity.y) && rb.velocity.y < 0)
+        {
+            selectedSprites = sSprites;
+            facing = 3;
+        }
+    }
+}
