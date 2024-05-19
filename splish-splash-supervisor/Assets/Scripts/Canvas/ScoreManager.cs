d32989980652a792b4c9afe77e46e10827c0e178
pgchmp22 <noaheh27@gmail.com>
2024-05-19 14:29:25 -0700

    idek

--- splish-splash-supervisor/Assets/Scripts/Canvas/ScoreManager.cs
+++ splish-splash-supervisor/Assets/Scripts/Canvas/ScoreManager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText; // Change to TextMeshProUGUI
     public int initialScore = 100;
     public int score;
+    public string endingName = "ending";
     
     // Start is called before the first frame update
     void Start()
@@ -32,6 +34,11 @@ public class ScoreManager : MonoBehaviour
     {
         score -= points;
         UpdateScoreText();
+
+        if(score <= 0)
+        {
+            SceneManager.LoadScene(endingName);
+        }
     }
 
     private void UpdateScoreText()
